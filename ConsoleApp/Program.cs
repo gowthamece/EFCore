14a1559375fe0f76ed8e61df3d8f4fe5b5d1c5fb
Gowtham K <gowthamk91@outlook.com>
2020-04-21 20:54:39 +0530

    Added CRUD Module

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,29 +10,56 @@ namespace ConsoleApp
         private static readonly AppDBContext context = new AppDBContext(); 
         static void Main(string[] args)
         {
-            context.Database.EnsureCreated();// Don't use this statement in production code
-            Console.WriteLine("---------Before Add----------");
-            GetUsers();
-            AddUser();            
-            Console.WriteLine("----------After Add-----------");
-            GetUsers();
+            // context.Database.EnsureCreated();// Don't use this statement in production code
+            //Console.WriteLine("---------Before Add----------");
+             GetUsers();
+            //   AddUser();            
+            //Console.WriteLine("----------After Add-----------");
+            //GetUsers();
+            // UpdateUser();
+           // RemoveUser();
+            //BatchUpdateUser();
             Console.WriteLine();
             Console.ReadKey();
         }
         public static void AddUser()
         {
-            var user = new User { Name = "Gowtham" };
-            context.Users.Add(user); // Add user record in database 
+            var user = new User { Name = "Ramesh" };
+            context.Users.Add(user); 
             context.SaveChanges();// Saves all changes made in this context to the database
         }
         public static void GetUsers()
-        {
+        {                    
             var users = context.Users.ToList();
-            Console.WriteLine($"User count is {users.Count}");
-            foreach(var user in users)
+            foreach (var user in users)
             {
                 Console.WriteLine(user.Name);
-            }
+            }           
         }
+        public static void UpdateUser()
+        {
+            var user = context.Users.Find(2);
+            user.Name += " Kumar";
+            context.Users.Update(user);
+            context.SaveChanges();
+        }
+        public static void BatchUpdateUser()
+        {
+            var users = context.Users.ToList();
+            users.ForEach(s => s.Name += "kumar");
+            //foreach (var user in users)
+            //{
+            //    user.Name += " Kumar";
+            //}
+            context.SaveChanges();
+        }
+
+        public static void RemoveUser()
+        {
+            var user = context.Users.Find(5);
+            context.Users.Remove(user);
+            context.SaveChanges();
+        }
+
     }
 }
--- EFCoreDemo.Data/AppDBContext.cs
+++ EFCoreDemo.Data/AppDBContext.cs
@@ -1,18 +1,30 @@
 ﻿using EFCoreDemo.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace EFCoreDemo.Data
 {
-    public class AppDBContext: DbContext
+    public class AppDBContext : DbContext
     {
-            public DbSet<User> Users { get; set; } // The User class is from Domain project
-            public DbSet<Skill> Skills { get; set; }// The Skill class is from Domain project
-            public DbSet<Rating> Ratings { get; set; }// The Rating class is from Domain project
-            public DbSet<UserSkill> UserSkills { get; set; } //Joining Entity 
+        public DbSet<User> Users { get; set; } // The User class is from Domain project
+        public DbSet<Skill> Skills { get; set; }// The Skill class is from Domain project
+        public DbSet<Rating> Ratings { get; set; }// The Rating class is from Domain project
+        public DbSet<UserSkill> UserSkills { get; set; } //Joining Entity 
+
+        public static readonly ILoggerFactory ConsoleLoggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+            .AddFilter((category, level) =>
+                category == DbLoggerCategory.Database.Command.Name
+                && level == LogLevel.Information)
+             .AddConsole();
+        }); 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=[Provide user SQL server Name]; Initial Catalog = EFCoreDemoDB; Integrated Security=SSPI"); // 
+            optionsBuilder
+                .UseLoggerFactory(ConsoleLoggerFactory)
+                .UseSqlServer("Data Source=[Provide user SQL server Name]; Initial Catalog = EFCoreDemoDB; Integrated Security=SSPI"); // 
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
