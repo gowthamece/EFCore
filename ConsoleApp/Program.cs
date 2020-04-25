4280f0bae1982c14ab9630318a39d9cae81a0f02
Gowtham K <gowthamk91@outlook.com>
2020-04-25 20:00:30 +0530

    Related data & Eager loading module added

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using EFCoreDemo.Data;
 using EFCoreDemo.Domain;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ConsoleApp
@@ -12,13 +14,18 @@ namespace ConsoleApp
         {
             // context.Database.EnsureCreated();// Don't use this statement in production code
             //Console.WriteLine("---------Before Add----------");
-             GetUsers();
+            // GetUsers();
             //   AddUser();            
             //Console.WriteLine("----------After Add-----------");
             //GetUsers();
             // UpdateUser();
-           // RemoveUser();
+            // RemoveUser();
             //BatchUpdateUser();
+            //AddUserWithRating();
+            //AddRatingtoExistingUsers();
+            // EargerLoadingWithRating();
+            //ProjectionLoadingWithRating();
+            QueryFilteringWithRating();
             Console.WriteLine();
             Console.ReadKey();
         }
@@ -28,6 +35,60 @@ namespace ConsoleApp
             context.Users.Add(user); 
             context.SaveChanges();// Saves all changes made in this context to the database
         }
+        public static void AddUserWithRating()
+        {
+            var user = new User
+            {
+                Name = "Saravanan",
+                
+                Rating = new Rating {RatingLevel="A"}
+                
+            };
+            context.Users.Add(user);
+            context.SaveChanges();// Saves all changes made in this context to the database
+        }
+        public static void AddRatingtoExistingUsers()
+        {
+            var user = context.Users.ToList();
+            context.Ratings.Add(new Rating
+            {
+                RatingLevel = "B",
+                Users = user
+            });
+            context.SaveChanges();
+        }
+        public static void EargerLoadingWithRating()
+        {
+            var ratings = context.Ratings.Include(r => r.Users).ToList();
+            var rating = context.Ratings.Include(r => r.Users).FirstOrDefault(u=>u.RatingLevel=="B");
+        }
+
+        public static void ProjectionLoadingWithRating()
+        {
+            //var ratings = context.Ratings.Select(s => new { s.RatingLevel, s.Users }).ToList();
+            var ratings = context.Ratings.Select(r => new { ratings=r , B_rating= r.Users.Where(q=>q.Name.Contains("gow"))}).ToList();
+
+            var rating = ratings[1].ratings.RatingLevel += "*";
+        }
+
+        public static void QueryFilteringWithRating()
+        {
+            //var ratings = context.Ratings.Select(s => new { s.RatingLevel, s.Users }).ToList();
+            var ratings = context.Ratings.Where(r => r.Users.Any(q=>q.Name.Contains("gow"))).ToList();
+
+            
+        }
+
+        //public static void AddRatingtoExistingUsers1()
+        //{
+        //    var rating = context.Ratings.Find(2);
+        //    rating.Users.Add(new User
+        //    {
+        //        Name="Ramu"
+        //    });
+        //    using (var newContext)
+        //    context.SaveChanges();
+        //}
         public static void GetUsers()
         {                    
             var users = context.Users.ToList();
