5f67511513167efa62f1b1cda1caf2e1a6e70102
Gowtham K <gowthamk91@outlook.com>
2020-04-26 16:44:17 +0530

    Entity Framwork with an ASP.NET Core Web application

--- EFCoreDemo.Data/AppDBContext.cs
+++ EFCoreDemo.Data/AppDBContext.cs
@@ -6,26 +6,35 @@ namespace EFCoreDemo.Data
 {
     public class AppDBContext : DbContext
     {
+        /** Comment this contructor to use console app **/
+        public AppDBContext(DbContextOptions<AppDBContext> options):base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
         public DbSet<User> Users { get; set; } // The User class is from Domain project
         public DbSet<Skill> Skills { get; set; }// The Skill class is from Domain project
         public DbSet<Rating> Ratings { get; set; }// The Rating class is from Domain project
         public DbSet<UserSkill> UserSkills { get; set; } //Joining Entity 
 
-        public static readonly ILoggerFactory ConsoleLoggerFactory = LoggerFactory.Create(builder =>
-        {
-            builder
-            .AddFilter((category, level) =>
-                category == DbLoggerCategory.Database.Command.Name
-                && level == LogLevel.Information)
-             .AddConsole();
-        }); 
+        
+        /** UnCommnet if for Console APP**/
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder
-                .UseLoggerFactory(ConsoleLoggerFactory)
-                .UseSqlServer("Data Source=[Provide user SQL server Name]; Initial Catalog = EFCoreDemoDB; Integrated Security=SSPI"); // 
-        }
+
+        //public static readonly ILoggerFactory ConsoleLoggerFactory = LoggerFactory.Create(builder =>
+        //{
+        //    builder
+        //    .AddFilter((category, level) =>
+        //        category == DbLoggerCategory.Database.Command.Name
+        //        && level == LogLevel.Information)
+        //     .AddConsole();
+        //}); 
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder
+        //        .UseLoggerFactory(ConsoleLoggerFactory)
+        //        .UseSqlServer("Data Source=[Provide user SQL server Name]; Initial Catalog = EFCoreDemoDB; Integrated Security=SSPI"); // 
+        //}
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
            modelBuilder.Entity<UserSkill>().HasKey(s => new { s.SkillId, s.UserId });
